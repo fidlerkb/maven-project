122f370f63fc2d32d2c73aadb18e6def999e7e0a
max <maxfid@gimmonix.com>
2020-01-22 07:51:59 +0200

    fix 2

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -13,7 +13,7 @@ public class Greeter {
   }
 
   /**
-   * @param someones name
+   * @param someone name of a poeson
    * @return greeting string
    */
   public final String greet(final String someone) {
